2b9b55ca004b2777e9c8bb90606fcf1252c91716
ryan <ryanmarchock@gmail.com>
2018-09-06 23:11:49 +0100

    reworking logic to use streams. Not completed

--- index.js
+++ index.js
@@ -1,29 +1,75 @@
 #!/usr/bin/env node
 
-const createJSONIcons = require('./src/JsonIcons').createJSONIcons;
-const saveToFile = require('./src/JsonIcons').saveToFile;
-const updateIconListWithFullPath = require('./src/JsonIcons').updateIconListWithFullPath;
+// const BasePath = process.cwd();
 
-const JSONFilePath = process.argv.slice(2)[0];
-const BasePath = process.cwd();
-const iconList = updateIconListWithFullPath(BasePath, JSONFilePath)
+const fs = require('fs')
+const through = require('through2')
+const svgson = require('svgson');
 
-/* Create JSON file with icon data
- *
- * @param {object} iconList
- * @param {func}
- * */
-createJSONIcons(
 
-    iconList,
+const readJSON = function(chunk, encoding, done) {
+    const data = JSON.parse(chunk.toString())
+    Object.entries(data).forEach(([key, value]) => {
+        const string = JSON.stringify({[key]: value})
+        this.push(string)
+    })
+    done()
+}
 
-    /* Callback function
-     *
-     * @param {object} iconList
-     * @param {string} path
-     * */
-    (iconList, path) => {
-        const icons = JSON.stringify(iconList);
-        saveToFile(icons, path);
+const getSVGList = function(chunk, encoding, done) {
+    const data = JSON.parse(chunk.toString())
+    const src = `.${Object.values(data)[0].input}`
+    const fileNames = fs.readdirSync(src)
+    const filePaths = fs.readdirSync(src).map((fileName) => src + fileName)
+    const key = Object.keys(data)[0]
+    const values = Object.values(data)[0]
+    const newData = {
+        ...data,
+        [key]: {
+            ...values,
+            svgFileNames: fileNames,
+            svgFilePaths: filePaths,
+        }
     }
-);
+    const string = JSON.stringify(newData)
+    this.push(string)
+    done()
+}
+
+const readSVG = function(chunk, encoding, done) {
+    const data = JSON.parse(chunk.toString())
+    const list = Object.values(data)[0].svgFilePaths
+    const options = { svgo: true, pathsKey: 'myPaths' };
+
+
+    list.forEach((fileName) => {
+        fs.readFile(fileName, 'utf8', (err, icon) => {
+            if (err) throw err;
+            // console.log(data);
+
+            svgson(icon, options, (result) => {
+                console.log(result.myPaths.childs)
+            });
+        });
+    })
+
+    // done()
+}
+
+
+const getJSON = (src) => {
+    const readStream = fs.createReadStream(src);
+    readStream
+        .pipe(through(readJSON))
+        .pipe(through(getSVGList))
+        .pipe(through(readSVG))
+}
+
+
+
+const json = getJSON('icons/icons.json')
+
+// https://github.com/jeresig/node-stream-playground
+
+
+
